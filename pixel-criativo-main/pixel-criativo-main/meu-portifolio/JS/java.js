4b4de4e9258920ac1a441f7e9158dfbd10236517
Victor Gabriel <victorgabrielbr1256@gmail.com>
2025-05-09 08:59:11 -0300

    deleted

--- pixel-criativo-main/pixel-criativo-main/meu-portifolio/JS/java.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const header = document.getElementById('meuHeader');
-    if (window.scrollY > 20) { // Defina a distância de rolagem (em pixels) para ativar a mudança
-      header.classList.add('scroll');
-    } else {
-      header.classList.remove('scroll');
-    }
-  });
